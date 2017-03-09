aa53cc896a05efc3510af0568525386f80215742
Camilo Ernesto Forero Junco <camilo.forero3@googlemail.com>
2017-03-09 09:01:40 -0500

    First commit

--- src/components/search.js
+++ src/components/search.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+export default class Search extends Component {
   render() {
     return (
       <div className="App">
@@ -13,9 +11,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <
       </div>
     );
   }
 }
-
-export default App;
